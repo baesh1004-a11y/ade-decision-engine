071b39dfe97b46bd8501b48354c29e930f49552d
baesh1004-a11y <baesh1004@gmail.com>
2026-07-24 21:04:49 +0900

    Apply premium theme to Korean trading desk

--- pages/9_Trading_Desk.py
+++ pages/9_Trading_Desk.py
@@ -1,8 +1,12 @@
+import streamlit as st
+
+from dashboard.ade_design_system import apply_premium_theme
 from trading.name_resolution_patch import install_name_resolution_patch
 from trading.order_service_patch import install_order_service_patch
 from dashboard.trading_desk_chart_first_app import run
 from dashboard.kis_realtime_panel import render_kis_realtime_panel
 
+apply_premium_theme(st, page="kr-trading")
 install_name_resolution_patch()
 install_order_service_patch()
 run()

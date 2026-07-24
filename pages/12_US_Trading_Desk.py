67cf4fc3dd19f6de56c6434430e7381d4c05894f
baesh1004-a11y <baesh1004@gmail.com>
2026-07-24 21:04:29 +0900

    Apply premium theme to US trading desk

--- pages/12_US_Trading_Desk.py
+++ pages/12_US_Trading_Desk.py
@@ -1,3 +1,7 @@
+import streamlit as st
+
+from dashboard.ade_design_system import apply_premium_theme
 from dashboard.us_trading_desk_app import run
 
+apply_premium_theme(st, page="us-trading")
 run()

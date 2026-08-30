3c443db5c6276517ba4f58355c209c52b3f3969b
baesh1004-a11y <baesh1004@gmail.com>
2026-08-30 19:25:30 +0900

    fix: wire replay watch to resilient historical loader

--- replay_target/__init__.py
+++ replay_target/__init__.py
@@ -1,3 +1,10 @@
+from . import integrated as _integrated
+from .kis_history import load_reference_history as _load_reference_history
+
+# Use the resilient KIS/Naver/KRX historical loader in the integrated workbench.
+# The function is looked up at run time by ReplayTargetIntegratedService.run_live.
+_integrated._load_reference_history = _load_reference_history
+
 from .integrated import IntegratedWatchConfig, IntegratedWatchResult, ReplayTargetIntegratedService
 from .path_watch import PathDailyMatch, PathWatchConfig, PathWatchSnapshot, build_path_snapshot
 from .watch import TargetWatchConfig, TargetWatchSnapshot, build_snapshot, classify_state
